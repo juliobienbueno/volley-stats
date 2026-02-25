ca0ecf18efae7a359c3572a40e7d5965558d0a1e
JulioNoteHuawei <julioignaciob@gmail.com>
2026-02-25 14:13:17 -0300

    Atributos de modelo agregados

--- backend/players/admin.py
+++ backend/players/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Player
 
 # Register your models here.
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    # Esto es para que en la lista veas más columnas y no solo el __str__
+    list_display = ('number', 'first_name', 'last_name', 'position')
+    # Permite buscar por nombre o número
+    search_fields = ('first_name', 'last_name', 'number')
--- backend/players/models.py
+++ backend/players/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+
+    POSITIONS = [
+        ('armador','Armador'),
+        ('punta','Punta'),
+        ('central','Central'),
+        ('opuesto','Opuesto'),
+        ('libero','Libero'),
+        ('universal','Universal / Sin posición')
+    ]
+
+
+    first_name = models.CharField(max_length=100 , verbose_name='Nombre del Jugador')
+    last_name = models.CharField(max_length=100 , verbose_name='Apellido del Jugador')
+    number = models.PositiveIntegerField(verbose_name='Número de Camiseta')
+
+    position = models.CharField(
+        max_length = 20,
+        choices = POSITIONS,
+        default='universal',
+        verbose_name="Posición"
+    )
+
+    #Devuelve el número el nombre y el apellido con el metodo str
+    def __str__(self):
+        return f"{self.number} - {self.first_name} {self.last_name}"
+    
+    class Meta:
+        verbose_name= "Jugador"
+        verbose_name_plural= "Jugadores" 
